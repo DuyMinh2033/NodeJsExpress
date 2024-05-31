5793570638641fba9fc340477076aed22de5c77e
DuyMinh2033 <DuyMinh2033@gmail.com>
2024-05-31 23:27:35 +0700

    Change some texts

--- src/app/controller/MeController.js
+++ src/app/controller/MeController.js
@@ -2,7 +2,7 @@ const { mutipleMongooseToObject } = require('../../util/mongose')
 const Course = require('../models/Course')
 class meController{
     store(req ,res , next ){
-        console.log(req)
+        
         Course.find({})
         .then(course => res.render('me/storedCourse',{
             courses: mutipleMongooseToObject(course)
